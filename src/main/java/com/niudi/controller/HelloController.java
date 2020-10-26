2feb7cfe95b815d3a8eb945c4a26382339ee2296
kongxiaoyang <kongxiaoyang@hzzl.net>
2020-10-26 15:33:58 +0800

    develop

--- src/main/java/com/niudi/controller/HelloController.java
+++ src/main/java/com/niudi/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
+ * 测试分支合并
  * @Author Administrator
  */
 @Controller
